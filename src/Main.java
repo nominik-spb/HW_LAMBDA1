81225ac863c292cadabf57b130f51dc1127c0d39
nominik <mike.nogai.spb@mail.ru>
2025-08-08 23:02:24 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Calculator calc = Calculator.instance.get();
 
         int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1,1);
+        int b = calc.minus.apply(1, 1);
 
         // ошибка деления на ноль
         // необходима проверка возможности вычисления
@@ -14,7 +14,11 @@ public class Main {
             int c = calc.devide.apply(a, b);
             calc.println.accept(c);
         } catch (Exception ex) {
-            System.out.println("Вычисление невозможно из-за несоответствия одного из параметров");
+            System.out.println("Вычисление невозможно из-за несоответствия одного из параметров (" + a + "/" + b + ")");
         }
+
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker worker = new Worker(listener);
+        worker.start();
     }
 }
